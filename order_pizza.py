e7756e9dbc3141dc200677f461d3f19f2324110d
jasoncwilley <jason.c.willey@gmail.com>
2016-08-29 12:01:33 -0400

    fixed display order and added explanations of code above each section

--- order_pizza.py
+++ order_pizza.py
@@ -1,3 +1,5 @@
+""" defines what pizzas are available"""
+
 PIZZAS = (
     {
         "name": "Cheese Pizza",
@@ -9,26 +11,44 @@ PIZZAS = (
     },
 )
 
-my_pizzas = []
 
+"""Stores list of pizzza customoer orders  """
+my_pizzas = []
+"""test function to see if input is a digit and vailid """
 def display_invalid_option(menu_selection):
     if menu_selection.isdigit():
         print("\n{} is an invalid option, please try again".format(menu_selection))
     else:
         print("\n{} is not a number, please enter a number from the menu above".format(menu_selection))
 
+
+"""checks to make sure that customers selection is one either cheese or pep """
 def is_valid_pizza(pizza_selection, pizzas):
     return pizza_selection.isdigit() and int(pizza_selection)-1 < len(pizzas)
 
-def add_to_order():
-    """
-    Prompts for adding pizza to the order
-    """
 
+""" diplays current order using index and the sum of the pizzas selected"""
+def display_pizzas(pizzas):
+	    if len(pizzas) > 0:
+        for index, pizza in enumerate(pizzas):
+            print("{}: {}  cost: ${}".format(index+1, pizza["name"], pizza["cost"]))
+    else:
+        print("No pizzas found.")
+
+"""Sums the number of pizzas and type ordered and sums up the cost"""
+def display_total_cost(pizzas):
+    total_cost = sum([pizza["cost"] for pizza in pizzas])
+    print("===========")
+    print("TOTAL COST: ${}".format(total_cost))
+
+
+""" Prompts the user to confirm add to order  """
+def add_to_order():
+    
+""" loop determines if the selection is valid, the type of pizza ordered """ 
     while True:
         print("\n")
-        for index, pizza in enumerate(PIZZAS):
-            print("{}: {}".format(index+1, pizza["name"]))
+        display_pizzas(PIZZAS)
         print("0: Go back")
 
         pizza_selection = input("\nWhich pizza would you like to order? ")
@@ -41,6 +61,8 @@ def add_to_order():
             display_invalid_option(pizza_selection)
 
 def main():
+   global my_pizzas
+
     MENU_ITEMS = (
         "1: Add Pizza to Order",
         "2: Remove Pizza from Order",
@@ -59,17 +81,18 @@ def main():
             break
         elif menu_selection == "1":
             add_to_order()
-            pass
         elif menu_selection == "2":
             pass
         elif menu_selection == "3":
-            pass
+            display_pizzas(my_pizzas)
+            display_total_cost(my_pizzas)
+            print("\n\n")
         elif menu_selection == "4":
-            pass
+        	if ordered_pizza():
+        	print 	my_pizzas():        
         else:
             display_invalid_option(menu_selection)
 
-
-
+         
 if __name__ == '__main__':
     main()
